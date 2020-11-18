fb18d2b56291bfa6d875dd070f438eac18d48394
gifteeintern <gifteeintern@gifteeinternnoMacBook-Pro.local>
2020-11-18 16:56:08 +0900

    apiを利用して感染者情報を取得する機能を追加

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,4 +1,7 @@
 require 'line/bot'
+require "uri"
+require "net/http"
+require "json"
 
 class WebhookController < ApplicationController
   protect_from_forgery except: [:callback] # CSRF対策無効化
@@ -35,7 +38,28 @@ class WebhookController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           if event.message['text'].in?(prefectures)
-            message['text'] = "#{event.message['text']}\n2020/11/18の感染者数\n200人\n累計感染者数\n4000人"
+            uri = URI.parse('https://covid19-japan-web-api.now.sh/api/v1/prefectures')
+            https = Net::HTTP.new(uri.host, uri.port);
+            https.use_ssl = uri.scheme === "https"
+
+            headers = { "Content-Type" => "application/json" }
+            request = Net::HTTP::Get.new(uri.path)
+            request.initialize_http_header(headers)
+            response = https.request(request)
+
+            covid_json = response.read_body
+            covid = JSON.parse(covid_json)
+            prefecture_id = prefectures.find_index { |n| n ==  event.message['text']}
+            
+            update_date = covid[prefecture_id]["last_updated"]["cases_date"]
+            update_date = update_date.to_s
+            update_date_year = update_date[0..3]
+            update_date_month = update_date[4..5]
+            update_date_day = update_date[6..]
+            
+            infected = covid[prefecture_id]["cases"] - covid[prefecture_id]["discharge"] - covid[prefecture_id]["deaths"]
+            
+            message['text'] = "#{event.message['text']}\n最終更新日\n#{update_date_year}/#{update_date_month}/#{update_date_day}\n現在の感染者数\n#{infected}人\n重症者数\n#{covid[prefecture_id]["severe"]}人\n死亡者数\n#{covid[prefecture_id]["deaths"]}人\n累計感染者数\n#{covid[prefecture_id]["cases"]}人"
           end
         end
         client.reply_message(event['replyToken'], message)
