c33d40dcd9ecd8d2386302a010d8cd65aa94896f
gifteeintern <gifteeintern@gifteeinternnoMacBook-Pro.local>
2020-11-18 18:49:00 +0900

    指摘箇所の修正

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -6,15 +6,19 @@ require "json"
 class WebhookController < ApplicationController
   protect_from_forgery except: [:callback] # CSRF対策無効化
 
+  PREFECTURES = [
+    '北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県','茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県','新潟県','富山県',
+    '石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県','三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県','鳥取県','島根県',
+    '岡山県','広島県','山口県','徳島県','香川県','愛媛県','高知県','福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県'
+  ]
+
+  API_URL = 'https://covid19-japan-web-api.now.sh/api/v1/prefectures'
+
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
-  end
-  
-  def infected_population(covid_info)
-    return covid_info["cases"] - covid_info["discharge"] - covid_info["deaths"]
   end
 
   def callback
@@ -24,8 +28,6 @@ class WebhookController < ApplicationController
     unless client.validate_signature(body, signature)
       head 470
     end
-
-    api_url = 'https://covid19-japan-web-api.now.sh/api/v1/prefectures'
 
     events = client.parse_events_from(body)
     events.each { |event|
@@ -38,32 +40,38 @@ class WebhookController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           if event.message['text'].in?(PREFECTURES)
-            uri = URI.parse(api_url)
+            uri = URI.parse(API_URL)
             response = Net::HTTP.get_response(uri)
-
-            covid_json = response.read_body
-            covid = JSON.parse(covid_json)
-            prefecture_id = PREFECTURES.find_index(event.message['text'])
+            body = response.read_body
             
-            update_date = covid[prefecture_id]["last_updated"]["cases_date"]
-            update_date = update_date.to_s
-            update_date = Date.parse(update_date)
-            
-            infected = infected_population(covid[prefecture_id])
+            covid = JSON.parse(body)
+            prefecture = event.message['text']
+            if prefecture != '北海道'
+              prefecture = prefecture.chop
+            end
 
-            message['text'] = <<~EOS
-              #{event.message['text']}
-              最終更新日
-              #{update_date.year}/#{update_date.month}/#{update_date.day}
-              現在の感染者数
-              #{infected}人
-              重症者数
-              #{covid[prefecture_id]["severe"]}人
-              死亡者数
-              #{covid[prefecture_id]["deaths"]}人
-              累計感染者数
-              #{covid[prefecture_id]["cases"]}人
-            EOS
+            for covid_prefecture in covid do
+              if covid_prefecture['name_ja'] == prefecture
+                update_date = covid_prefecture["last_updated"]["cases_date"]
+                update_date = update_date.to_s
+                update_date = Date.parse(update_date)
+
+                message['text'] = <<~EOS
+                  #{event.message['text']}
+                  最終更新日
+                  #{update_date.strftime("%Y/%m/%d")}
+                  現在の感染者数
+                  #{infected_population(covid_prefecture)}人
+                  重症者数
+                  #{covid_prefecture["severe"]}人
+                  死亡者数
+                  #{covid_prefecture["deaths"]}人
+                  累計感染者数
+                  #{covid_prefecture["cases"]}人
+                EOS
+                break
+              end
+            end
           end
         end
         client.reply_message(event['replyToken'], message)
@@ -71,4 +79,10 @@ class WebhookController < ApplicationController
     }
     head :ok
   end
+
+  private
+  def infected_population(covid_info)
+    return covid_info["cases"] - covid_info["discharge"] - covid_info["deaths"]
+  end
+
 end
